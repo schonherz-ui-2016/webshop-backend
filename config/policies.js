fae1b4f69bcb4ec2b41253d055ab7b0246984b5f
Zsolt Gyongyosi <Zsolt_Gyongyosi@epam.com>
2016-12-05 16:45:28 +0100

    Opened products' and categories' list

--- config/policies.js
+++ config/policies.js
@@ -3,5 +3,13 @@ module.exports.policies = {
   '*': 'auth',
 	UserController: {
     'login': true
+  },
+  ProductController: {
+    'find': true,
+    'findOne': true
+  },
+  CategoryController: {
+    'find': true,
+    'findOne': true
   }
 };
